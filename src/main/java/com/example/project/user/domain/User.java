70e000946d2cbbcfc827f34e7856114ce6c6381a
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-06-10 19:15:05 +0900

    length 수정

--- src/main/java/com/example/project/user/domain/User.java
+++ src/main/java/com/example/project/user/domain/User.java
@@ -11,24 +11,32 @@ import javax.persistence.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 255 , nullable = false)
+    @Column(length = 255, nullable = false)
     private String accountId;
 
-    @Column(length = 64 , nullable = false)
+    @Column(length = 60, nullable = false)
     private String password;
 
-    @Column(length = 20 , nullable = false)
+    @Column(length = 20, nullable = false)
     private String name;
 
-    @Column(length = 6 , nullable = false)
+    @Column(length = 6, nullable = false)
     private Long studentId;
 
     @Builder
     public User(String accountId, String password, String name, Long studentId) {
+        this.accountId = accountId;
+        this.password = password;
+        this.name = name;
+        this.studentId = studentId;
+    }
+
+    public void update(String accountId, String password, String name, Long studentId) {
         this.accountId = accountId;
         this.password = password;
         this.name = name;
