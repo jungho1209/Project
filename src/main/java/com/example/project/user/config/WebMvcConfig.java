7da00b25957fe45b1674c493c9fa38697b168b34
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-06-11 15:36:35 +0900

    ⚙ :: Cors 해결

---
+++ src/main/java/com/example/project/user/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.example.project.user.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// todo @CrossOrigin 구현
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
