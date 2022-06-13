359d3a94ee063903dbc0095031edcb6573440167
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-06-14 08:13:49 +0900

    🐳 :: Exception 처리 형식 변경

--- src/main/java/com/example/project/user/error/ErrorResponse/BindErrorResponse.java
+++ src/main/java/com/example/project/user/error/ErrorResponse/BindErrorResponse.java
@@ -5,14 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE) // todo AllArgs 랑 Builder 같이 쓰는 이유 ? 빌더 패턴은 파라미터를 1개씩 가지는 생성자가 필요한데 이를 AllArgs 가 만들어줌
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+// todo AllArgs 랑 Builder 같이 쓰는 이유 ? 빌더 패턴은 파라미터를 1개씩 가지는 생성자가 필요한데 이를 AllArgs 가 만들어줌
 @Builder
 
 public class BindErrorResponse {  // todo BindError 는 @Valid 와 연관이 있음.
 
     private final int status;
-    private final List<String> messages;
+    private final String messages;
 }
--- src/main/java/com/example/project/user/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/project/user/error/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.example.project.user.error;
 
-import com.example.project.user.error.ErrorResponse.BindErrorResponse;
 import com.example.project.user.error.ErrorResponse.CustomErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +8,14 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 
-@RestControllerAdvice // todo 컨트롤러 전역에서 발생할 수 있는 예외를 잡아서 처리
+@RestControllerAdvice // todo 컨트롤러 전역에서 발생할 수 있는 예외를 잡아서 처리(@RestController 어노테이션 붙은 클래스만)
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(CustomException.class) // todo 컨트롤러 어노테이션 적용된 Bean 내에서 발생하는 예외 잡아서 하나의 메소드에서 처리
+    @ExceptionHandler(CustomException.class) // todo 컨트롤러 어노테이션 적용된 Bean 내에서 발생하는 예외 잡아서 하나의 메소드에서 처리 하나의 클래스만 처리
     public ResponseEntity<CustomErrorResponse> customExceptionHandling(CustomException e) {
         final ErrorCode errorCode = e.getErrorcode();
         return new ResponseEntity<>(
@@ -28,18 +27,13 @@ public class GlobalExceptionHandler {
         );
     }
 
-    @ExceptionHandler(BindException.class) // todo BindException -> @valid 와 연관 검증
-    public ResponseEntity<BindErrorResponse> bindExceptionHanding(BindException e) {
-        List<String> errorsList = new ArrayList<>(); // error 담는 리스트
-        // todo 에러를 가져와서 에러 리스트에  메세지를 얻어와서 추가 ?
+    @ExceptionHandler(BindException.class) // todo BindException -> @valid 와 연관
+    public ResponseEntity<?> bindExceptionHanding(BindException e) {
+        Map<String, String> errorsList = new HashMap<>(); // todo error 담는 리스트 key - value 형태로 저장되는 배열 : Map
         for (FieldError error : e.getFieldErrors()) { // todo error 의 길이만큼 반복하여 메세지를 추가한다
-            errorsList.add(error.getDefaultMessage());
+            errorsList.put(error.getField(), error.getDefaultMessage());
         }
 
-        BindErrorResponse errorResponse = BindErrorResponse.builder()
-                .status(HttpStatus.BAD_REQUEST.value())
-                .messages(errorsList)
-                .build();
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorsList, HttpStatus.BAD_REQUEST);
     }
 }
